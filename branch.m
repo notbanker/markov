fa0238ddda2b3366ae5faefce2419912dd398de6
Peter Cotton <2006@petercotton.net>
2013-01-04 13:53:21 -0500

    Deleted extraneous readme etc. See +table/README.txt instead

--- branch.m
+++
@@ -1,9 +0,0 @@
-function thisDirectory = branch
-% Put this at the top level of the modules directory (synomym for branch)
-    if (~isdeployed)
-        w = which('branch');
-        thisDirectory = w(1:end-length('branch.m')-1);
-    else
-        thisDirectory = pwd;
-    end
-end
