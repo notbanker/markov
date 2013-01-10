b3b58cf51c0ac171ff912bd5cddfaee3b16a7b70
Peter Cotton <2006@petercotton.net>
2013-01-10 09:47:25 -0600

    Removed extraneous file

--- +table/+select/unique.m
+++ +table/+select/unique.m
@@ -1,7 +1,12 @@
-function uds = unique(ds,varargin)
+function uds = unique(ds,fieldsToGroupBy,fieldsToShow)
 % Supply a list of fields. Returns a table where every row contains a unique combination
 % of values taken by these fields (the last occurance is chosen). 
-[K,U,R,O] = table.indexing.createKey(table.deal.toVarargout(ds,varargin{:}));
+vals = table.deal.toNumericArray(ds,fieldsToGroupBy);
+
+[K,U,R,O] = table.indexing.createKey(  );
 uds = table.select.rows(ds,O); 
+if nargin>=3,
+   uds = table.select.cols(uds,fieldsToShow); 
+end
 end
 
