096b313204c1616accd6d51016301ccb6dc2799c
Peter Cotton <2006@petercotton.net>
2013-01-30 22:41:30 -0500

    Hopefully fixed the silly problem with the third argument

--- +table/+indexing/previousOccurances.m
+++ +table/+indexing/previousOccurances.m
@@ -1,4 +1,4 @@
-function I = previousOccurances(key,lags,nUniqueKeys,C)
+function I = previousOccurances(key,lags,keyMax,C)
 % I=previousOccurances(key,lags) returns a matrix with length(key) rows and
 % length(lags) columns. 
 %   If lags(k)=1, column I(i,k) reports the previous position j satisfying key(j)==key(i). 
@@ -21,25 +21,18 @@ function I = previousOccurances(key,lags,nUniqueKeys,C)
 %      3     8
 %
 % lags can be any vector of desired lags such as [1,2,4,8]
-% nUniqueKeys is optional and indicates the number of unique keys (actually it may be
-% larger than the number of unique keys, though this may lead to inefficiency). 
-% 
-% C is optional and defaults to (1..nUniqueKeys). C can be used to report the previous
-% position for a *different* key rather than the key which appears. This is mildly confusing
-% and documented in createKeyChoice.tex. The notation 'C' is shared by 
+%
+% The remaining two optional arguments can assist performance. 
+%    keyMax must be greater than equal to the largest key value, if provided   
+%    C defaults to (1..keyMax). 
+%
+% C can be used to report the previous position for a *different* key rather than the key which appears.
+% This is mildly confusing and documented in createKeyChoice.tex. The notation 'C' is shared by 
 % TABLE.LAGGED.VALUESOFANOTHER and TABLE.INDEXING.CREATEKEYCHOICE, incidentally. For an
 % example of this more advanced usage see TABLE.TEST.UNITTEST_LAGGEDVALUESOFANOTHER
 
 % TODO: This function is a bottleneck and might be rewritten as mex
 
-if length(nUniqueKeys)>1,
-    warning('You may be using an old form of this function where U was supplied instead of nUniqueKeys');
-    nUniqueKeys = size(nUniqueKeys,1);
-end
-
-if nargin<4,
-    C = (1:nUniqueKeys)';
-end
 
 if ~isvector(lags) || any(lags<=0) || any(abs(ceil(lags)-lags)>1e-6),
     error('Expecting positive integer lags');
@@ -48,13 +41,21 @@ end
 if b~=1,
     error('Expecting vector key');
 end
-if nargin>=3,
-    nKinds = nUniqueKeys;
-else
-    nKinds = length(unique(key(1:min(500,a))))+10; % Reason apparent in loop below
+
+if nargin>=3 && length(keyMax)>1,
+    warning('You may be using an old form of this function where U was supplied instead of keyMax');
+    keyMax = size(keyMax,1);
 end
+if nargin<3,
+    keyMax = length(unique(key(1:min(500,a))))+10; % Reason apparent in loop below
+end
+
+if nargin<4,
+    C = (1:keyMax)';
+end
+
 maxLags = max(lags);
-prevI = nan(nKinds,maxLags);
+prevI = nan(keyMax,maxLags);
 I = nan(a,length(lags));
 if 0, % alternative algo, slightly faster because vectorized, but still clobbers cache?
     % ** doesn't handle C() yet **
@@ -78,8 +79,7 @@ if nargin>=3,
         prevI(key(k),1) = k;
     end
 else
-    warning('Using a slower loop. It is strongly recommended that the third parameter J be supplied.');
-    % Loop is repeated here because it might be slower
+    warning('Using a slower loop. It is strongly recommended that the third parameter (keyMax) be supplied.');
     for k=1:a,
         if key(k)>size(prevI,1),
             prevI = [prevI;nan(key(k)+10-size(prevI,1),maxLags)]; % expand lookup if necessary
